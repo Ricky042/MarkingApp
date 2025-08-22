797c27f9e4f94a3d4b31af1477dd1039c0ac707e
kshang1 <kerryshang31@gmail.com>
2025-08-22 13:01:40 +1000

    Setting up shadcn

--- frontend-vite/vite.config.js
+++ frontend-vite/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    resolve: { alias: { "@": "/src" } },
+    resolve: { alias: { "@": path.resolve(__dirname, "./src"), } },
     server: {
       proxy: {
         '/signup': 'http://localhost:5000',
